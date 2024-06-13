72ab654384c3eba59ebc72841f8c4a5cf7a1b092
anuragsingh1990 <61200616+anuragsingh1990@users.noreply.github.com>
2024-06-13 07:51:43 +0000

    number and date

---
+++ 01_basics/date_var.js
@@ -0,0 +1,7 @@
+const date = new Date()
+console.log(date.toString())
+console.log(date.toDateString())
+
+console.log(date.toTimeString())
+
+console.log(typeof date)
---
+++ 01_basics/number_and_maths.js
@@ -0,0 +1,26 @@
+const num =new Number(600093)
+console.log(num.toString().length)
+console.log(num.toFixed(2))
+
+const desiNum=new Number(89.9812)
+console.log(desiNum.toFixed(2))
+console.log(num.toLocaleString())
+
+console.log(num.toLocaleString('en-IN'))
+
+
+const hundreds=10000000
+
+//for getting upper side value
+console.log(Math.ceil(45.1))
+
+//for getting lower side value
+console.log(Math.floor(45.8))
+
+//to calculate the power of number
+console.log(Math.pow(5,3))
+
+
+
+//
+console.log(Math.min(6,9,12,15,4,5,2,7))
